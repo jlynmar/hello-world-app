ac5e117e971ec8f684ef25fd44de17557a08b9b3
jlynmar <jeralynmartinez03@gmail.com>
2025-04-29 17:20:42 -0700

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/hello-world-app/' : '',
+  basePath: isProd ? '/hello-world-app' : '',
+  output: 'export'
+};
 
 export default nextConfig;
